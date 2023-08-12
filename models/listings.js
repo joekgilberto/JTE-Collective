0222c6defc161e58d839a173b18c53db8c5506f1
joegilberto <joekgilberto@gmail.com>
2023-08-12 15:13:12 -0400

    created models/listings.js

---
+++ models/listings.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: {
+        type: Buffer,
+        contentType: String
+        //TODO: default
+    },
+    listingDate: {
+        type: Date,
+        default: new Date()
+    },
+    sold: {
+        type: Boolean,
+        default: false
+    },
+    auctions: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Auction'
+    }]
+})
+
+module.exports = mongoose.model('Listing', listingSchema)
