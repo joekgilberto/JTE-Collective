a31b2c6776d6a6d01f249b1729f293e72bbcfa1e
joegilberto <joekgilberto@gmail.com>
2023-08-12 15:40:09 -0400

    index and new routes connected

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var methodOverride = require('method-override');
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var listingsRouter = require('./routes/listings');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/listings', listingsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
